6dfcf4b3df3dc3ca3fb489bb68388098855c52b9
harsithaamilpur <110337259+harsithaamilpur@users.noreply.github.com>
2022-08-04 23:36:33 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+
+http.createServer((req, res) => {
+   res.writeHead(200, { 'Content-Type': 'text/html' })
+   res.write("Test App is working")
+   res.end()
+}).listen(port, () => {
+   console.log('Running');
+})
